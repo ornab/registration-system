c4d089f07f65e83ee637aff410664d711ad6ec19
ornab <hzamil17@gmail.com>
2016-05-11 23:41:18 +0600

    my second commit

---
+++ class.user.php
@@ -0,0 +1,94 @@
+<?php
+
+require_once('dbconfig.php');
+
+class USER
+{	
+
+	private $conn;
+	
+	public function __construct()
+	{
+		$database = new Database();
+		$db = $database->dbConnection();
+		$this->conn = $db;
+    }
+
+   
+	public function start($sql)
+	{
+		$db = $this->conn->prepare($sql);
+		return $db;
+	}
+	
+	public function register($uname,$umail,$upass)
+	{
+		try
+		{
+			$new_password = password_hash($upass, PASSWORD_DEFAULT);
+			
+			$stmt = $this->conn->prepare("INSERT INTO users(user_name,user_email,user_pass) 
+		                                               VALUES(:uname, :umail, :upass)");
+												  
+			$stmt->bindparam(":uname", $uname, PDO::PARAM_STR);
+			$stmt->bindparam(":umail", $umail, PDO::PARAM_STR);
+			$stmt->bindparam(":upass", $new_password, PDO::PARAM_STR);										  
+				
+			$stmt->execute();	
+			
+			return $stmt;	
+		}
+		catch(PDOException $e)
+		{
+			echo $e->getMessage();
+		}				
+	}
+	
+	
+	public function doLogin($uname,$umail,$upass)
+	{
+		try
+		{
+			$stmt = $this->conn->prepare("SELECT user_id, user_name, user_email, user_pass FROM users WHERE user_name=:uname OR user_email=:umail ");
+			$stmt->execute(array(':uname'=>$uname, ':umail'=>$umail));
+			$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+			if($stmt->rowCount() == 1)
+			{
+				if(password_verify($upass, $userRow['user_pass']))
+				{
+					$_SESSION['user_session'] = $userRow['user_id'];
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+		catch(PDOException $e)
+		{
+			echo $e->getMessage();
+		}
+	}
+	
+	public function is_loggedin()
+	{
+		if(isset($_SESSION['user_session']))
+		{
+			return true;
+		}
+	}
+	
+	public function redirect($url)
+	{
+		header("Location: $url");
+	}
+	
+	public function doLogout()
+	{
+		session_destroy();
+		unset($_SESSION['user_session']);
+		return true;
+	}
+}
+?>
