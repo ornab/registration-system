32f0d98a5aade1698e2e495245799b1d857ace63
ornab <hzamil17@gmail.com>
2016-05-11 23:42:38 +0600

    my third commit

---
+++ dblogin.sql
@@ -0,0 +1,12 @@
+ 
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(15) NOT NULL,
+  `user_email` varchar(40) NOT NULL,
+  `user_pass` varchar(255) NOT NULL,
+  `joining_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+ 
