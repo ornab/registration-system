95bc7bba8c6c5c8c45cad4bab5e8de99a427a3f7
ornab <hzamil17@gmail.com>
2016-05-11 23:43:46 +0600

    my fourth commit

---
+++ home.php
@@ -0,0 +1,35 @@
+<?php
+
+	require_once("session.php");
+	
+	require_once("class.user.php");
+	$auth_user = new USER();
+	
+	
+	$user_id = $_SESSION['user_session'];
+	
+	$stmt = $auth_user->start("SELECT * FROM users WHERE user_id=:user_id");
+	$stmt->execute(array(":user_id"=>$user_id));
+	
+	$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+<link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
+<script type="text/javascript" src="jquery-1.11.3-jquery.min.js"></script>
+<link rel="stylesheet" href="style.css" type="text/css"  />
+<title>welcome - <?php print($userRow['user_email']); ?></title>
+</head>
+
+<body>
+  
+       <h1>You Have Successfully Logged in! </h1>
+        <a href="logout.php?logout=true"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Sign Out</a>
+
+         
+</body>
+</html>
